354775174193eb0d1fe947625b83d86b9d0f050e
Star We <starwe@Stars-MacBook-Pro.local>
2017-03-22 15:42:49 +0800

    delete bower_components

--- static/app/webroot/js/master.js
+++ static/app/webroot/js/master.js
@@ -2,6 +2,12 @@ var app = angular.module("myApp", []);
 
 app.controller("getTime", function($scope, $filter) {
   var format;  //时间格式
-
   $scope.time = new Date();
+
+  config(['$routeProvider', function($routeProvider){
+    $routeProvider.
+      when('/',{templateUrl: 'common/main.html'}).
+      otherwise({redirectTo: '/'});
+  }]);
+  
 });
