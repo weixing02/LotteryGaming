acb53783df94de11f560efe136847972ee542b0d
Star We <starwe@Stars-MacBook-Pro.local>
2017-03-27 17:35:41 +0800

    页面不再搓一起

--- static/app/webroot/js/master.js
+++ static/app/webroot/js/master.js
@@ -1,13 +1,14 @@
-var app = angular.module("myApp", []);
+var app = angular.module("myApp",['ngRoute']);
+
+app.config(function($routeProvider){
+  $routeProvider.
+    when('/',{templateUrl: 'common/main.html'}).
+    when('/about',{templateUrl: 'common/about.html'}).
+    otherwise({redirectTo: '/'});
+});
 
 app.controller("getTime", function($scope, $filter) {
   var format;  //时间格式
   $scope.time = new Date();
 
-  config(['$routeProvider', function($routeProvider){
-    $routeProvider.
-      when('/',{templateUrl: 'common/main.html'}).
-      otherwise({redirectTo: '/'});
-  }]);
-  
 });
